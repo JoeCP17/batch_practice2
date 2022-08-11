4e683309a86d82dbb0ddc1ce4a8e42e3670d4068
Ueibin <ats1113@zuminternet.com>
2022-08-11 22:21:34 +0900

    Spring Batch 동기테스트

---
+++ src/main/java/com/example/springbatchpractice2/JobLauncherController.java
@@ -0,0 +1,40 @@
+package com.example.springbatchpractice2;
+// 동기적 & 비동기적 예제 적용
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class JobLauncherController {
+
+    @Autowired
+    private Job job;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @PostMapping("/batch")
+    public String launch(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("id", member.getId())
+                .addDate("Date", new Date())
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+
+        return "batch Completed";
+    }
+}
