895845d5d13a76b78215a5412102352453310f23
Ueibin <ats1113@zuminternet.com>
2022-08-11 23:05:13 +0900

    Spring Batch 동기 & 비동기 테스트

--- src/main/java/com/example/springbatchpractice2/JobLauncherController.java
+++ src/main/java/com/example/springbatchpractice2/JobLauncherController.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +25,12 @@ public class JobLauncherController {
     @Autowired
     private Job job;
 
+//    @Autowired
+//    private JobLauncher jobLauncher;
+
+    // 비동기 진행시 필요한 항목
     @Autowired
-    private JobLauncher jobLauncher;
+    private BasicBatchConfigurer basicBatchConfigurer;
 
     @PostMapping("/batch")
     public String launch(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
@@ -33,6 +40,8 @@ public class JobLauncherController {
                 .addDate("Date", new Date())
                 .toJobParameters();
 
+        SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
         jobLauncher.run(job, jobParameters);
 
         return "batch Completed";
